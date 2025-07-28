c9ea9a75bbbca10098a6d8734b6a24fc6c372b56
Vishwajeet Tripathi <tripathi.vishwajeet12345@gmail.com>
2025-07-28 10:38:21 +0530

    added scroll restoration , updated browser router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,14 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { LandingPage } from './features/quiz/components/LandingPage';
-import { QuizInput } from './features/quiz/components/QuizInput';
-import { QuizPage } from './features/quiz/components/QuizPage';
-import { ResultPage } from './features/quiz/components/ResultPage';
+import { Routes, Route, Outlet, ScrollRestoration } from 'react-router-dom';
 
 
+// render the current page (Outlet) and provide scroll restoration.
 const App = () => {
     return (
-        <Routes>
-            <Route path='/' element={ <LandingPage/> } />
-            <Route path='/quizInput' element={ <QuizInput/> } />
-            <Route path='/quizPage' element={ <QuizPage/> } />
-            <Route path='/quizResult' element={ <ResultPage/> } />
-        </Routes>
+        <>
+            <Outlet />
+            <ScrollRestoration />
+        </>
     );
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
 import { Provider } from 'react-redux';
 // import appStore from './appStore.js';
@@ -8,15 +8,46 @@ import CssBaseline from '@mui/material/CssBaseline';
 import App from './App.jsx';
 import theme from './theme.js';
 
+// importing components
+import { LandingPage } from './features/quiz/components/LandingPage';
+import { QuizInput } from './features/quiz/components/QuizInput';
+import { QuizPage } from './features/quiz/components/QuizPage';
+import { ResultPage } from './features/quiz/components/ResultPage';
+
+
+// defining routes
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <LandingPage />,
+      },
+      {
+        path: '/quizInput',
+        element: <QuizInput />,
+      },
+      {
+        path: '/quizPage',
+        element: <QuizPage />,
+      },
+      {
+        path: '/quizResult',
+        element: <ResultPage />,
+      },
+    ],
+  },
+]);
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <Provider store = {appStore} > */}
-      <BrowserRouter>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <App />
-        </ThemeProvider>
-      </BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     {/* </Provider> */}
   </React.StrictMode>,
 );
